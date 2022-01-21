7b29f74f60cd529433a20c85c8988f56865463c1
Shovan Dhara <sdhara@infoblox.com>
2022-01-21 20:11:51 +0530

    commit for state hooks

--- src/customhooks/hookComponent.js
+++ src/customhooks/hookComponent.js
@@ -1,6 +1,7 @@
 import { ThemeContext, themes } from '../themeContext';
 import React, { Fragment } from 'react';
 import useClickOutside from './clickOutsideHook';
+import useStore from './storeHook';
 
 function CustomHooks() {
     const value = React.useContext(ThemeContext);
@@ -10,6 +11,7 @@ function CustomHooks() {
                 The current theme is || {value === themes.dark ? 'dark' : 'light'}
             </div>
             <Menu />
+            <Todos />
         </Fragment>
 
     )
@@ -29,5 +31,26 @@ function Menu() {
         </div>
     )
 }
+
+function Todos() {
+    const [{ todos }, setStore] = useStore();
+    const getMaxId = todos.reduce(function (prev, current) {
+        return (prev.id > current.id) ? prev : current
+    })
+    const addTodos = () => {
+        setStore(old => ({
+            ...old,
+            todos: [...old.todos, { task: 'Brand New Todos', id: getMaxId + 1 }]
+        }))
+    }
+    return (
+        <ul>
+            {todos.map((item) => {
+                return <li key={item.id}>{item.task}</li>
+            })}
+        </ul>
+    )
+}
+
 
 export default CustomHooks;
